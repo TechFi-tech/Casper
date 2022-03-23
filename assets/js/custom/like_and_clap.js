b25bff32ad1d880f44c7c64f61b8658cfbe38a8b
tranduynhat0209 <nhatducmo@gmail.com>
2022-03-23 11:38:11 +0700

    fix: url, separate files, boostrap

---
+++ assets/js/custom/like_and_clap.js
@@ -0,0 +1,78 @@
+const postId = document.getElementById("postId").innerHTML
+const userId = document.getElementById("userId").innerHTML
+        
+        if(TechFiIntegrationBaseURL){
+        
+        const getUrl = TechFiIntegrationBaseURL + "/posts?postId=" + postId + "&userId=" + userId
+        const xhttp = new XMLHttpRequest();
+        xhttp.onload = function() {
+            res = JSON.parse(this.responseText)
+            const like = res?.postInfo?.like ? res.postInfo.like : 0;
+            const clap = res?.postInfo?.clap ? res.postInfo.clap : 0;
+
+            document.getElementById("like-num").innerHTML = like
+            document.getElementById("clap-num").innerHTML = clap
+
+            const liked = res?.postMember?.like ? res.postMember.like : 0;
+            const clapped = res?.postMember?.clap ? res.postMember.clap: 0;
+
+            document.getElementById("liked").innerHTML = liked
+            document.getElementById("clapped").innerHTML = clapped
+            if(clapped > 0){
+                document.getElementById("Capa_1").setAttribute("fill", 'red')
+                if(clapped >= 30)
+                document.getElementById("Capa_1").setAttribute("fill", "grey")
+            }
+            document.getElementById("Layer_1").setAttribute("fill", liked > 0 ? 'red': 'black')
+        }
+        xhttp.open("GET", getUrl);
+        xhttp.send();
+    }
+        function clap(){
+            if(!TechFiIntegrationBaseURL) return;
+            const postURL = TechFiIntegrationBaseURL + "/posts/clap"
+            const api = new XMLHttpRequest();
+            
+            
+            api.onload = function(){
+                res = JSON.parse(this.responseText)
+                const clap = res?.postInfo?.clap;
+                const clapped = res?.postMember?.clap;
+                if(clapped){
+                    document.getElementById("clapped").innerHTML = clapped
+                    if(clapped > 0){
+                        document.getElementById("Capa_1").setAttribute("fill", 'red')
+                        if(clapped >= 30)
+                    document.getElementById("Capa_1").setAttribute("fill", "grey")
+                    }
+                }
+                if(clap)
+                    document.getElementById("clap-num").innerHTML = clap
+            }
+            api.open("POST", postURL)
+            api.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            api.send(`postId=${postId}&userId=${userId}`)
+        }
+        function like(){
+            //if(!TechFiIntegrationBaseURL) return;
+            const postURL = "http://localhost:8000/api/v1.0/posts/like"
+            console.log(postURL)
+            const api = new XMLHttpRequest();
+            
+            api.onload = function(){
+                res = JSON.parse(this.responseText)
+                const like = res?.postInfo?.like;
+                const liked = res?.postMember?.like;
+                if(liked !== undefined){
+                    document.getElementById("liked").innerHTML = liked
+                    
+                    document.getElementById("Layer_1").setAttribute("fill", liked === 0 ? 'black': 'red')
+                }
+                if(like)
+                    document.getElementById("like-num").innerHTML = like
+            }
+            api.open("POST", postURL, true)
+
+            api.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            api.send(`postId=${postId}&userId=${userId}`)
+        }
