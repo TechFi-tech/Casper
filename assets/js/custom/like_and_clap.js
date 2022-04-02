3ed62b646ee02c05ff0cd2f5906c5cd0d910488d
linlin <nguyenthanh2303@gmail.com>
2022-04-02 10:24:53 +0700

    fix: update un signin user

--- assets/js/custom/like_and_clap.js
+++ assets/js/custom/like_and_clap.js
@@ -1,9 +1,9 @@
 const postId = document.getElementById("postId").innerHTML
-const userId = document.getElementById("userId").innerHTML
+const userId = document.getElementById("userId").innerHTML || "0"
 document.getElementById("Capa_1").setAttribute("fill", '#062225')
 document.getElementById("Like_1").setAttribute("fill", '#062225')
         if(TechFiIntegrationBaseURL){
-        
+
         const getUrl = TechFiIntegrationBaseURL + "/posts?postId=" + postId + "&userId=" + userId
         const xhttp = new XMLHttpRequest();
         xhttp.onload = function() {
@@ -16,7 +16,7 @@ document.getElementById("Like_1").setAttribute("fill", '#062225')
 
             const liked = res?.postUser?.like ? res.postUser.like : 0;
             const clapped = res?.postUser?.clap ? res.postUser.clap: 0;
-            
+
             document.getElementById("liked").innerHTML = liked
             document.getElementById("clapped").innerHTML = clapped
             if(clapped > 0){
@@ -24,7 +24,7 @@ document.getElementById("Like_1").setAttribute("fill", '#062225')
                 if(clapped >= 30)
                 document.getElementById("Capa_1").setAttribute("fill", "#3b3b3b")
             }
-            
+
             document.getElementById("Like_1").setAttribute("fill", liked > 0 ? '#00bea5': '#062225')
         }
         xhttp.open("GET", getUrl);
@@ -38,15 +38,15 @@ document.getElementById("Like_1").setAttribute("fill", '#062225')
             if(!TechFiIntegrationBaseURL) return;
             const postURL = TechFiIntegrationBaseURL + "/posts/clap"
             const api = new XMLHttpRequest();
-            
-            
+
+
             api.onload = function(){
                 res = JSON.parse(this.responseText)
                 const clap = res?.postInfo?.clap;
                 const clapped = res?.postUser?.clap;
                 if(clapped){
                     document.getElementById("clapped").innerHTML = clapped
-                    
+
                     if(clapped > 0){
                         document.getElementById("Capa_1").setAttribute("fill", '#00bea5')
                         if(clapped >= 30)
@@ -65,14 +65,14 @@ document.getElementById("Like_1").setAttribute("fill", '#062225')
             const postURL = TechFiIntegrationBaseURL + "/posts/like"
             console.log(postURL)
             const api = new XMLHttpRequest();
-            
+
             api.onload = function(){
                 res = JSON.parse(this.responseText)
                 const like = res?.postInfo?.like;
                 const liked = res?.postUser?.like;
                 if(liked !== undefined){
                     document.getElementById("liked").innerHTML = liked
-                    
+
                     document.getElementById("Like_1").setAttribute("fill", liked > 0 ? '#00bea5': '#062225')
                 }
                 if(like !== undefined)
@@ -84,5 +84,5 @@ document.getElementById("Like_1").setAttribute("fill", '#062225')
             api.send(`postId=${postId}&userId=${userId}`)
         }
         function save(){
-            
+
         }
