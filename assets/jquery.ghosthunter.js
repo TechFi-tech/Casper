f2894dd68bc2802fc4bb61a7cb2c150e2b4aa035
anhphantq <phananhtqvn@gmail.com>
2022-04-14 22:53:54 +0700

    fix search box responsive

--- assets/jquery.ghosthunter.js
+++ assets/jquery.ghosthunter.js
@@ -3250,7 +3250,7 @@
     onPageLoad: false,
     onKeyUp: false,
     result_template:
-      "<a id='gh-{{ref}}' class='gh-search-item search-result-box-item' href='{{link}}'><p>{{title}}</p></a>",
+      "<div id='gh-{{ref}}' class='gh-search-item search-result-box-item'><div class='gh-search-hit-icon' style='display:inline-block; margin-left:5px;margin-right:15px;'><svg width='20' height='20' viewBox='0 0 20 20'><path d='M17 6v12c0 .52-.2 1-1 1H4c-.7 0-1-.33-1-1V2c0-.55.42-1 1-1h8l5 5zM14 8h-3.13c-.51 0-.87-.34-.87-.87V4' stroke='currentColor' fill='none' fill-rule='evenodd' stroke-linejoin='round'></path></svg></div> <a class='search-result-box-item-link' href='{{link}}'>{{title}}</a> </div>",
     info_template: "<p>Number of posts found: {{amount}}</p>",
     displaySearchInfo: true,
     zeroResultsInfo: true,
