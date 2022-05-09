668b859b8c70e80aba5f3bb695e4c3a9b9014623
linlin <nguyenthanh2303@gmail.com>
2022-05-09 09:51:45 +0700

    fix: reset localstorage if change ghost search version

---
+++ assets/js/custom/ghost_search.js
@@ -0,0 +1,13 @@
+const GHOST_SEARCH_VESION = '1.0.0';
+const GHOST_SEARCH_VESION_KEY = 'GHOST_SEARCH_VESION_KEY';
+var localVersion = localStorage.getItem(GHOST_SEARCH_VESION_KEY);
+if (!localVersion || localVersion !== GHOST_SEARCH_VESION) {
+    for (const key in localStorage) {
+        if (key.startsWith('ghost')) {
+            console.log(`${key}: ${localStorage.getItem(key)}`);
+            localStorage.setItem(key, '');
+        }
+
+    }
+    localStorage.setItem(GHOST_SEARCH_VESION_KEY, GHOST_SEARCH_VESION);
+}
