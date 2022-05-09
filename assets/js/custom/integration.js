5f583f4d85de00d2f9ad72fd8a0474e53e93f8d9
linlin <nguyenthanh2303@gmail.com>
2022-05-09 12:01:53 +0700

    feat: update get jwt at profile

--- assets/js/custom/integration.js
+++ assets/js/custom/integration.js
@@ -1,6 +1,7 @@
 let integrationFrontendDefault = 'https://staging.i-frontend.techfi.tech';
 // let integrationFrontendDefault = 'http://localhost:3000';
 let integrationFrontend = integrationFrontendDefault;
+const INTEGRATION_JWT_MEMBER_KEY = 'INTEGRATION_JWT_MEMBER';
 try {
     integrationFrontend = integrationFrontendURL;
 } catch (e) {
@@ -11,28 +12,34 @@ function addValueToAttr(elementId, attrKey, attrValue) {
     document.getElementById(elementId).setAttribute(attrKey, attrValue);
 }
 
-function httpGet(url, body) {
+function httpPost(url, body, headers) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open('POST', url, false); // false for synchronous request
-    xmlHttp.setRequestHeader("Accept", "application/json");
-    xmlHttp.setRequestHeader("Content-Type", "application/json");
+    xmlHttp.setRequestHeader('Accept', 'application/json');
+    xmlHttp.setRequestHeader('Content-Type', 'application/json');
+    for (var key in headers) {
+        xmlHttp.setRequestHeader(key, headers[key]);
+    }
+
     xmlHttp.send(JSON.stringify(body));
     return xmlHttp.responseText;
 }
 
 const ghostIntegrationJwtUrl = '/members/api/integration-member-jwt';
 var TechFiIntegrationBaseURL = TechFiIntegrationBaseURL || 'http://localhost:8000';
+getIntegrationMemberJwtToken;
 
 function getIntegrationMemberJwtToken() {
     var host = window.location.host;
     var protocol = location.protocol;
     var url = `${protocol}//${host}${ghostIntegrationJwtUrl}`;
-    console.log(url);
+    // console.log(url);
     var integrationJwtUrl = TechFiIntegrationBaseURL + '/users/jwt-ghost-member';
 
-    console.log(integrationJwtUrl);
-    var body = {integrationJwtUrl: integrationJwtUrl};
-    let value = httpGet(url, body);
-
+    // console.log(integrationJwtUrl);
+    var headers = {integrationJwtUrl: integrationJwtUrl};
+    var body = {};
+    let value = httpPost(url, body, headers);
+    localStorage.setItem(INTEGRATION_JWT_MEMBER_KEY, value);
     return value;
 }
