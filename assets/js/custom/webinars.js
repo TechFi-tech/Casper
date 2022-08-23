3a8dbeb89bee96e6f45519fb4d144b298fa9e405
linlin <nguyenthanh2303@gmail.com>
2022-08-23 10:10:17 +0700

    feat: hide excerpt

--- assets/js/custom/webinars.js
+++ assets/js/custom/webinars.js
@@ -1,74 +1,78 @@
 function parseJwt(token) {
-  var base64Url = token.split(".")[1];
-  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-  var jsonPayload = decodeURIComponent(
-    atob(base64)
-      .split("")
-      .map(function (c) {
-        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-      })
-      .join("")
-  );
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(
+        atob(base64)
+            .split('')
+            .map(function (c) {
+                return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+            })
+            .join('')
+    );
 
-  return JSON.parse(jsonPayload);
+    return JSON.parse(jsonPayload);
 }
 
 var webinarSaved = false;
-var webinarID = parseJwt(
-  localStorage.getItem("INTEGRATION_JWT_MEMBER") ||
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmeSI6IjYyNjk1NmU3YzliODBhZGRmODhmODljYiIsInJvbGVzIjpbInVzZXIiXSwibWV0YURhdGEiOnsiZW1haWwiOiJwaGFuYW5odHF2bkBnbWFpbC5jb20ifSwiaWF0IjoxNjU0NzYwNTI1LCJleHAiOjE2NzAzMTI1MjV9.d_MWM_2UEYjyxUE-MbpbHFnpLigfsL3gcUnhTtNp_mo"
-).identify;
+var INTEGRATION_JWT_MEMBER = localStorage.getItem('INTEGRATION_JWT_MEMBER');
+var webinarID = parseJwt(INTEGRATION_JWT_MEMBER).identify;
 var webinarURL = location.href;
 
 async function checkSavedWebinar() {
-  const URL = `https://integration.techfi.tech/staging/api/v1.0/webinars/saved/${webinarID}`;
-  let res = await fetch(URL, { method: "GET" }).then((res) => res.json());
+    const URL = `${TechFiIntegrationBaseURL}/webinars/user/saved`;
+    const headers = {'Authorization': INTEGRATION_JWT_MEMBER};
+    let res = await fetch(URL, {method: 'GET', headers: headers}).then((res) => res.json());
 
-  res = res.webinar;
+    res = res.webinar;
 
-  res.forEach((e) => {
-    e.links.forEach((e) => {
-      if (e == webinarURL) {
-        webinarSaved = true;
-      }
+    res.forEach((e) => {
+            if (e.url === webinarURL) {
+                webinarSaved = true;
+            }
+
     });
-  });
 
-  if (webinarSaved) {
-    document.querySelector("#cls-1").setAttribute("fill", "#00bea5");
-  } else document.querySelector("#cls-1").setAttribute("fill", "grey");
+    if (webinarSaved) {
+        document.querySelector('#cls-1').setAttribute('fill', '#00bea5');
+    } else {
+        document.querySelector('#cls-1').setAttribute('fill', 'grey');
+    }
 }
 
 async function saveWebinar() {
-  const URL = "https://integration.techfi.tech/staging/api/v1.0/webinars";
+    const URL = `${TechFiIntegrationBaseURL}/webinars`;
+    const headers = {'Authorization': INTEGRATION_JWT_MEMBER};
+    if (!webinarSaved) {
+        const URL = `${TechFiIntegrationBaseURL}/webinars/user/save`;
 
-  if (!webinarSaved) {
-    const URL = `https://integration.techfi.tech/staging/api/v1.0/webinars/save/webinar`;
+        let res = await fetch(URL, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
 
-    let res = await fetch(URL, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
+            body: JSON.stringify({user_id: webinarID, webinar_link: webinarURL})
+        });
+        if (res.status != 200) {
+            return;
+        }
+        document.querySelector('#cls-1').setAttribute('fill', '#00bea5');
+        webinarSaved = true;
+    } else {
+        const URL = `${TechFiIntegrationBaseURL}/webinars/unsave/webinar`;
 
-      body: JSON.stringify({ user_id: webinarID, webinar_link: webinarURL }),
-    });
-    if (res.status != 200) return;
-    document.querySelector("#cls-1").setAttribute("fill", "#00bea5");
-    webinarSaved = true;
-  } else {
-    const URL = `https://integration.techfi.tech/staging/api/v1.0/webinars/unsave/webinar`;
+        let res = await fetch(URL, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
 
-    let res = await fetch(URL, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-
-      body: JSON.stringify({ user_id: webinarID, webinar_link: webinarURL }),
-    });
-    if (res.status != 200) return;
-    document.querySelector("#cls-1").setAttribute("fill", "grey");
-    webinarSaved = false;
-  }
+            body: JSON.stringify({user_id: webinarID, webinar_link: webinarURL})
+        });
+        if (res.status != 200) {
+            return;
+        }
+        document.querySelector('#cls-1').setAttribute('fill', 'grey');
+        webinarSaved = false;
+    }
 }
